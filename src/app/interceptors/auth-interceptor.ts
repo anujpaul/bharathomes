06750edc516ccef6196e82323514e0f18b66118e
Anuj Paul <anujpaul19@outlook.com>
2026-05-02 18:40:20 -0500

    Started adding upload property capability

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -22,7 +22,10 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       if (!token) return next(req);
 
       return next(req.clone({
-        setHeaders: { Authorization: `Bearer ${token}` }
+        setHeaders: { 
+          Authorization: `Bearer ${token}`,
+          'Ocp-Apim-Subscription-Key': 'd1be26c49a51476dbc485c1510149204'
+        }
       }));
     })
   );
