e427363975c5f3e29c1389901e36530d4949d136
Anuj Paul <anujpaul19@outlook.com>
2026-06-14 18:21:21 -0500

    azure deploy

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,6 +1,7 @@
 window.__config = {
   // baseUrl: "http://localhost:8080", // when using container
-  baseUrl: "http://localhost:5000",  //when using local dotnet run
+  // baseUrl: "http://localhost:5000",  //when using local dotnet run
+  baseUri: "https://bharathomes-service.azurewebsites.net"
 
   // blobBaseUrl: "https://bharathomes.blob.core.windows.net/production"
 };
